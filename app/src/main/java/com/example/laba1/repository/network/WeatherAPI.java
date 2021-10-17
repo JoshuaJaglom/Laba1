722dbeb0f1da43a81bc7180f8fd34790a6efde31
Zhukov Artyom <6238233@gmail.com>
2021-10-17 18:23:34 +0300

    Готовая третья лаба version 1.0

--- app/src/main/java/com/example/laba1/repository/network/WeatherAPI.java
+++ app/src/main/java/com/example/laba1/repository/network/WeatherAPI.java
@@ -4,6 +4,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface WeatherAPI {
-    @GET("onecall?lat=55.751125&lon=37.617831&exclude=hourly&appid=c5af8c5386619c27509740b802c70a47")
+    @GET("onecall?lat=55.751125&lon=37.617831&exclude=hourly&appid=c5af8c5386619c27509740b802c70a47&units=metric")
     Call<MyWeather> getWeather();
 }
